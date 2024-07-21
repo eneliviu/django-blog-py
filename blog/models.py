58427c217aec52c185a58a82bb7676660e5fb6be
Liviu Theodor Ene <eneliviu@gmail.com>
2024-07-21 15:28:44 +0000

    Add data model class for Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
+# A draft is defined as 0 and published as 1 
+# The default is to save as a draft (STATUS == 0).
 
 # Create your models here.
+class Post(models.Model):
+    
+    title = models.CharField(max_length=200, unique=True)
+    # The title values should be unique to avoid having blog posts
+    #  of the same name confusing your users.
+    
+    slug = models.SlugField(max_length=200, unique=True)
+    # the slug is what is used to build a URL for each post
+    
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts'
+    )
+    # One user can write many posts, so this is a one-to-many or Foreign Key. 
+    # The cascade on delete means that on the deletion of the user entry,
+    # all their posts are also deleted.
+
+    content = models.TextField()
+    # This is the blog article content.
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    # auto_now_add=True means the default created time is the time of post entry
+
+    status = models.IntegerField(choices=STATUS, default=0)
+    # this uses a constant STATUS
+
