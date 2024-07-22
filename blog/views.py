4d5fb0593bf401fc71b31a8dbedf8e9422f1fc81
Liviu Theodor Ene <eneliviu@gmail.com>
2024-07-22 17:36:28 +0000

    Add comments to views.

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,6 @@ from .models import Post
 
 # Class-based view that inherits from the generic.ListView class to display all posts.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all()  # a collection of records from the database.
+    #queryset = Post.objects.all().order_by('created_on')
     template_name = "post_list.html"
