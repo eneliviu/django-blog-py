0b4b6b5702c1a068918bc1518b4e857895937b4b
Liviu Theodor Ene <eneliviu@gmail.com>
2024-07-23 12:09:32 +0000

    Add django summernote text editor

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import Post, Comment # 'models' module in the same directory as admin.py
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
-admin.site.register(Post)
-# Allows to create, update and delete blog posts from the admin panel.
 # Each app has its own admin.py file
+
+
+# Allows to create, update and delete blog posts from the admin panel.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_field = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 admin.site.register(Comment)
