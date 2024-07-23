3ead9567a4ab6c5ede9d30638d61b4d9d2525363
Liviu Theodor Ene <eneliviu@gmail.com>
2024-07-23 16:52:05 +0000

    Add script.js to static

--- codestar/settings.py
+++ codestar/settings.py
@@ -157,7 +157,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # collect all static files from all apps in 'staticfiles' directory. 
+# Django will serve static files from 'staticfiles' directory.
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
