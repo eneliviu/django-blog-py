0275dd126f9acec154e35f6d796b6c5dd6849165
Liviu Theodor Ene <eneliviu@gmail.com>
2024-07-24 12:24:25 +0000

    update comments

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,12 @@ from django.urls import path
 # This file is where we'll list our blog app-specific URLs.
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name='post_detail')
+    path('', views.PostList.as_view(), name='home'),  # class-based view, requires as_view()
+    path('<slug:slug>/', views.post_detail, name='post_detail')  # url points to post_detail function-based view
+    # '<slug:slug>/' is where the slug value is passed from the template's URL tag. 
+    # This urlpattern creates a url path of the domain path plus the slug value.
+    # The slug is used for the blog post path as it uniquely identifies the post and 
+    # contains only characters valid for a URL.
+    # slug before colon -> data type
+    # slug after colon -> the post.slug value passed from the template (seen in the  browser bar)
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,9 @@ from .models import Post
 
 # Create your views here.
 
-# Class-based view that inherits from the generic.ListView class to display all posts.
+# CLASS-BASED view that inherits from the generic.
+# ListView class to display all posts.
+# Requires as_view() in the urls.py
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)  # a collection of records from the database.
     template_name = "blog/index.html"
@@ -13,7 +15,8 @@ class PostList(generic.ListView):
 
 def post_detail(request, slug):
     """
-    Display an individual :model:`blog.Post`.
+    Function-based view:
+    Displays an individual :model:`blog.Post`.
 
     **Template:**
     
@@ -23,9 +26,14 @@ def post_detail(request, slug):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
 
+
     return render(
         request,
         "blog/post_detail.html",
-        {'post': post},
+        {'post': post},  # {'post': post} dictionary is called context
+                         # The context passes data from view to template
+                         # The key name would be the same as the variable name passing through, 
+                         # e.g. {"post": post}.
     )
+
 
