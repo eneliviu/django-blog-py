9b108b1bd5eac9a338b43baf2cf30d235ca24f1a
Liviu Theodor Ene <eneliviu@gmail.com>
2024-07-24 10:49:44 +0000

    added post_detail view

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 # This file is where we'll list our blog app-specific URLs.
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail')
 ]
